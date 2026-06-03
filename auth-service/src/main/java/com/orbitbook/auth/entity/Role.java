c79ba66751febe5385b26c128520b393b243dc4c
JhowQT <jhon.q.torrez@gmail.com>
2026-06-02 22:31:25 -0300

    classes entity

---
+++ auth-service/src/main/java/com/orbitbook/auth/entity/Role.java
@@ -0,0 +1,36 @@
+package com.orbitbook.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ROLE")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_ROLE")
+    private Long idRole;
+
+    @Column(name = "NAME_ROLE", nullable = false, length = 100)
+    private String nameRole;
+
+    @Column(name = "DESCRIPTION", nullable = false, length = 200)
+    private String description;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    
+}
---
+++ auth-service/src/main/java/com/orbitbook/auth/entity/UserOrbit.java
@@ -0,0 +1,41 @@
+package com.orbitbook.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USERS_ORBIT")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserOrbit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USERS_ORBIT")
+    private Long idUsersOrbit;
+
+    @Column(name = "NAME", nullable = false, length = 260)
+    private String name;
+
+    @Column(name = "EMAIL", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "PASSWORD_HASH", nullable = false, length = 100)
+    private String passwordHash;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_ROLE", nullable = false)
+    private Role role;
+}
