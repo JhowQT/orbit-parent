ce9e526da960b4b0b925fa7638e2b63f8d935351
JhowQT <jhon.q.torrez@gmail.com>
2026-06-05 22:04:04 -0300

    criaçã quase do CRUD

--- booking-service/src/main/java/com/orbitbook/booking/config/FeignConfig.java
+++ booking-service/src/main/java/com/orbitbook/booking/config/FeignConfig.java
@@ -1,5 +1,11 @@
 package com.orbitbook.booking.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(
+        basePackages = "com.orbitbook.booking.feign"
+)
 public class FeignConfig {
-    
 }
